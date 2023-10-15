4bba0715eb475816392006723537dba1b14ef193
Rohit Prakash <rohitp934@gmail.com>
2023-10-15 15:56:41 -0400

    Updated lazyvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,1 +1,2 @@
-require("rpc")
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
---
+++ nvim/lua/config/keymaps.lua
@@ -0,0 +1,24 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local function map(mode, lhs, rhs, opts)
+  local keys = require("lazy.core.handler").handlers.keys
+  ---@cast keys LazyKeysHandler
+  -- do not create the keymap if a lazy keys handler exists
+  if not keys.active[keys.parse({ lhs, mode = mode }).id] then
+    opts = opts or {}
+    opts.silent = opts.silent ~= false
+    if opts.remap and not vim.g.vscode then
+      opts.remap = nil
+    end
+    vim.keymap.set(mode, lhs, rhs, opts)
+  end
+end
+
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+map("n", "<leader>a", mark.add_file, { desc = "Mark file (Harpoon)" })
+map("n", "<S-h>", ui.toggle_quick_menu)
+map("n", "<C-q>", function() ui.nav_prev() end)
+map("n", "<C-e>", function() ui.nav_next() end)
